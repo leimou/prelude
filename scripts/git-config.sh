959102bcb9c30351ce3fe89c5632ed8b1ade3719
Lei Mou <lei.mou.uu@gmail.com>
2014-10-14 18:08:07 +0800

    Add configuration routine for git and github account.

---
+++ scripts/git-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# configuration routine for git
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global color.ui true
+git config --global push.default simple
+git config --global user.name "Lei Mou"
+git config --global user.email "lei.mou.uu@gmail.com"
