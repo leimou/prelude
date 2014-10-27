9f4a0fa17a89430858351796916992023d95f6ee
Lei Mou <lei.mou.uu@gmail.com>
2014-10-27 17:16:32 +0800

    Add openssh-server to prerequisites list.

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y git emacs24 apt-file g++
+sudo apt-get install -y git emacs24 apt-file g++ openssh-server
 
 md ~/Github
 git clone https://github.com/ggreer/the_silver_searcher.git ~/Github
