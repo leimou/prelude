bafa83bd2b0ffe46a904dc8ebb90da5673fb2a9d
Lei Mou <lei.mou.uu@gmail.com>
2015-04-17 21:50:05 +0800

    Add wireshark to prerequisites

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,4 +1,6 @@
-sudo apt-get install -y git emacs24 apt-file g++ openssh-server
+sudo apt-get install -y git emacs24 apt-file g++ openssh-server cscope wireshark
 
 md ~/Github
 git clone https://github.com/ggreer/the_silver_searcher.git ~/Github
+
+sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $(which dumpcap)
