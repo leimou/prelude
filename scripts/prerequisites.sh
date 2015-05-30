8a125ff328cea6b62863847ae76391a9cef521da
Lei Mou <lei.mou.uu@gmail.com>
2015-05-30 16:17:24 +0800

    Update bootstrap script.

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,7 +1,6 @@
-sudo apt-get install -y git emacs24 apt-file g++ openssh-server cscope wireshark dos2unix clang manpages-posix-dev
-
-md ~/Github
-git clone https://github.com/ggreer/the_silver_searcher.git ~/Github
-git clone https://github.com/leimou/goodies.git ~/Github
+sudo apt-get install -y git emacs24 apt-file g++ openssh-server cscope wireshark dos2unix clang manpages-posix-dev terminator silversearcher-ag
 
 sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $(which dumpcap)
+
+cp .bash* ~/
+. ~/.bashrc
