64e275c89259a5b8febd78c9a9cd6fc13e51032e
Lei Mou <lei.mou.uu@gmail.com>
2015-06-02 21:36:16 +0800

    Rename prerequisite.sh to bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,5 +5,9 @@ sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $(which dumpcap)
 echo "Downloading redis ..."
 wget http://download.redis.io/releases/redis-3.0.1.tar.gz
 
+echo "Setting up bash ..."
 cp .bash* ~/
 . ~/.bashrc
+
+echo "Setting up git ..."
+. git-config.sh
