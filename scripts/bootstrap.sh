e46325eea80640784e411d12714e2096097d74da
Lei Mou <lei.mou.uu@gmail.com>
2016-07-13 20:47:50 +0800

    New customization

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,13 +1,10 @@
-sudo apt-get install -y git emacs24 apt-file g++ openssh-server cscope wireshark dos2unix clang manpages-posix-dev terminator silversearcher-ag cmake autoconf libtool meld
-
-sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $(which dumpcap)
-
-echo "Downloading redis ..."
-wget http://download.redis.io/releases/redis-3.0.1.tar.gz
+echo "Setting up git ..."
+. git-config.sh
 
 echo "Setting up bash ..."
 cp .bash* ~/
 . ~/.bashrc
 
-echo "Setting up git ..."
-. git-config.sh
+sudo apt-get install -y git emacs24 apt-file g++ openssh-server cscope wireshark dos2unix clang manpages-posix-dev terminator silversearcher-ag cmake autoconf libtool meld
+
+sudo setcap 'CAP_NET_RAW+eip CAP_NET_ADMIN+eip' $(which dumpcap)
